08165800dbeb3492eece77a671bb1bef1f51328f
Rajesh Sharma <rkumar1310@gmail.com>
2023-06-18 08:18:12 +0530

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Layout } from '../src/components/dom/Layout'
 import './global.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Dictate3D - AI 3D editor',
@@ -18,6 +19,16 @@ export default function RootLayout({ children }) {
       <body>
         {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
         <Layout>{children}</Layout>
+        <Script src='https://www.googletagmanager.com/gtag/js?id=G-X7CMVH7YYV' strategy='afterInteractive' />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-X7CMVH7YYV');
+        `}
+        </Script>
       </body>
     </html>
   )
